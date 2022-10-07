25040319a2babb3af532211288d40d73c51fdd82
Kevin04c <kevincespedes2004@gmai.com>
2022-10-06 20:17:17 -0500

    añandiendo el formato base64 a la imagenes

--- src/routes/ecommer.routes.js
+++ src/routes/ecommer.routes.js
@@ -4,12 +4,19 @@ import { cache } from "../middleware/cache.js";
 
 const router = Router();
 
-router.get("/api/ecommerce/products",cache, (req, res) => {
+router.get("/api/ecommerce/products", cache, (req, res) => {
   const sql = "SELECT * FROM PRODUCTOS";
   connection.query(sql, (err, result) => {
     if (err) {
       throw err;
     }
+    for (const product of result) {
+      const b64 = Buffer.from(product.imagenProducto).toString("base64");
+      const mimeType = "image/png"; // e.g., image/png
+      const url = `data:${mimeType};base64,${b64}`;
+      product.imagenProducto = url;
+    }
+    
     res.send(result);
   });
 });
